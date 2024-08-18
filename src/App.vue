99fb6b85cea96c4fe0b4afd57fdf3e40705ae8f1
yc-lin <z36322820@gmail.com>
2024-08-19 04:36:01 +0800

    建立Todo.vue，新增非同步載入資料功能

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-    
+     <!-- 當元件是aync時，用suspense包住，以達到層次化的非同步控制 -->
+    <suspense>
+      <template #default>
+        <Todo />
+      </template>
+      <template #fallback>
+        <div>Loading...</div>
+      </template>
+    </suspense>
   </div>
 </template>
 
 <script setup>
+import Todo from './views/Todo.vue';
 
 </script>
 
---
+++ src/views/Todo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{ todos }}
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const todos = ref([]);
+
+todos.value = await fetch(`http://127.0.0.1:8003/news`)
+    .then(res => res.json());
+
+</script>
+
+<style lang="scss" scoped></style>
